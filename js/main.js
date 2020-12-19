94574ba4707f001dfe66e6c08675c7dfc36f3229
Tsukasa INOUE <>
2020-12-19 09:57:10 +0900

    jquery edited

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 let ids = ['#about', '#skill', '#service', '#works', '#contact'];
 
 $(function(){
-    $('a[href^="#about"]').click(function(){
-      var speed = 500;
+    $('a[href^="#"]').click(function(){
+      var speed = 400;
       var href= $(this).attr("href");
-      var target = $(href == "#about" || href == "" ? 'html' : href);
+      var target = $(href == "#" || href == "" ? 'html' : href);
       var position = target.offset().top;
       $("html, body").animate({scrollTop:position}, speed, "swing");
       return false;
